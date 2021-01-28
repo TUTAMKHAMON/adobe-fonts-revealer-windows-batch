cd45214611fd91ca86f53a7e22eea9748ef66160
Anibaaal <Anibaaaal@gmail.com>
2021-01-28 13:58:52 -0300

    Update reveal.sh
    Add choice for Microsoft & Local user types.

--- reveal.sh
+++ reveal.sh
@@ -1,8 +1,21 @@
-#!/bin/sh
+#!/bin/bash
 
-echo "Font Revealer for Adobe Creative Cloud (Windows Version)"
+username=$USER
+microsoftusername=${username:0:5}
 
-cd /mnt/c/Users/$USER/AppData/Roaming/Adobe/CoreSync/plugins/livetype/r/
+printf  "\nFont Revealer for Adobe Creative Cloud (Windows)\n\nSelect your user type and username to find the path to Adobe Fonts folder.\nYou can check C:/Users folder to see your actual username.\nMicrosoft account username takes the first 5 chars from email\n\nPlease choose your username:\n\n  1) Microsoft account user (Username: $microsoftusername)\n  2) Local user (Username: $username)\n\nYour choice (1/2): "
+read account
+
+if [ "$account" != "${account#[1]}" ] ;then
+	accounttype=$microsoftusername
+    printf "\nSelected Microsoft user account.\n\n"
+else
+	accounttype=$username
+    printf "\nSelected Local user account.\n\n"
+fi
+
+
+cd /mnt/c/Users/$accounttype/AppData/Roaming/Adobe/CoreSync/plugins/livetype/r/
 
 for file in *
 
@@ -12,15 +25,14 @@ do
   fontName=`otfinfo --info ${file} | fgrep "PostScript name:" | grep -oE "[^ ]+$"`
   
   # create a FONT dir within downloads, if there is none
-  mkdir -p /mnt/c/Users/$USER/Downloads/FONTS/
+  mkdir -p /mnt/c/Users/$accounttype/Downloads/FONTS/
   
-  # copy files
-  cp $file "/mnt/c/Users/$USER/Downloads/FONTS/$fontName.otf"
+  # copy files (if no dry-run)
+  cp $file "/mnt/c/Users/$accounttype/Downloads/FONTS/$fontName.otf"
   
-  # echo changes:
-  echo "$file\t⮕\t$fontName.otf"
+# print changes:
+  printf "$file\t--->\t$fontName.otf\n"
   
 done
 
-echo "DONE! All fonts downloaded to C:/User/$USER/Downloads/FONTS".
-
+printf "\nDONE!\nAll fonts downloaded to C:/User/$accounttype/Downloads/FONTS".
